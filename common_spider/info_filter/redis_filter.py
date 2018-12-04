49c6d50af1281e9d55144a8b758e05d4043cddda
wiseljj <39580089+wiseljj@users.noreply.github.com>
2018-12-04 11:50:03 +0800

    Update redis_filter.py

--- common_spider/info_filter/redis_filter.py
+++ common_spider/info_filter/redis_filter.py
@@ -1,7 +1,9 @@
 import redis
 from . import BaseFilter
+#利用redis去重
 class RedisFilter(BaseFilter):
     def _get_storage(self):
+        #建立连接池
         pool=redis.ConnectionPool(host=self.redis_host,port=self.redis_port,db=self.redis_db)
         client=redis.StrictRedis(connection_pool=pool)
         return client
